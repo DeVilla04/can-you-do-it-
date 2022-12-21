2450f434e78500ca092e8d7191ef85a830c99b38
DeVilla04 <dalalvraj21@gmail.com>
2022-12-21 12:13:21 -0500

    added the MyAnswer function

--- CodeQuest.py
+++ CodeQuest.py
@@ -10,13 +10,20 @@ from helper import *
 
 
 def MyAnswer(self):
-   # please put in your answer  here
-   answer = ""
-   print(answer)
+    # please put in your answer  here
+    current = self.headval
+    prev = None
+    dupl_dict = {}
+    answer = ""
+    while current:
+        if current.dataval not in dupl_dict:
+            answer += current.dataval
+            dupl_dict[current.dataval] = None
+            prev = current
+        else:
+            prev.nextval = current.nextval
+        current = current.nextval
+    print(answer)
 
 
 MyAnswer(list)
-
-
-
-
